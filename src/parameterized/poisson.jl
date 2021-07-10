71e025f41834c56e38f306f976b95e95fd176d5b
Kusti Skytén <kskyten@users.noreply.github.com>
2021-07-10 08:41:38 +0300

    Update Poisson
    - add a @kwstruct for the log parametrization
    - add aliases for the parameters
    - add parameter transformations with `asparams`
    - add a distproxy

--- src/parameterized/poisson.jl
+++ src/parameterized/poisson.jl
@@ -6,6 +6,25 @@ using SpecialFunctions: logfactorial
 
 @parameterized Poisson(λ) ≪ CountingMeasure(ℤ[0:∞])
 
+@kwstruct Poisson(logλ)
+
+@kwalias Poisson [
+    mean      => λ
+    # μ         => λ
+    # mu        => μ
+    logmean   => logλ
+    loglambda => logλ
+    # logμ      => logλ
+    # logmu     => logμ
+]
+
+# asparams(::Type{<:Poisson}, ::Val{:μ}) = asℝ₊
+asparams(::Type{<:Poisson}, ::Val{:λ}) = asℝ₊
+asparams(::Type{<:Poisson}, ::Val{:logλ}) = asℝ
+# asparams(::Type{<:Poisson}, ::Val{:logμ}) = asℝ
+
+distproxy(d::Poisson{(:λ,)}) = Dists.Poisson(d.λ)
+
 Base.eltype(::Type{P}) where {P<:Poisson} = Int
 
 function logdensity(d::Poisson{(:λ,)}, y)
